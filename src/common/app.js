7c0f7c9450c31d3794842014fcc190705b92b0af
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2026-04-17 18:10:03 -0400

    Start migrating to Astro

--- src/common/app.js
+++
@@ -1,7 +0,0 @@
-export const axios = require('axios').create({
-    baseURL: process.env.VUE_APP_API_BASE ?? 'https://api.cosmicjs.com/v1/cole-portfolio',
-    responseType: 'json',
-    params: {
-        read_key: process.env.VUE_APP_READ_KEY
-    }
-})
